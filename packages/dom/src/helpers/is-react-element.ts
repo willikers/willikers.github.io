8177a4a7bd3b4667166dfdab3e719fc682baca1d
Ryan Naddy <untuned20@gmail.com>
2024-09-07 11:12:39 -0500

    pages update

--- packages/dom/src/helpers/is-react-element.ts
+++
@@ -1,9 +0,0 @@
-import { ReactElement, ReactNode } from 'react';
-
-export const isReactElement = (element: ReactNode): element is ReactElement => {
-  return !!element && typeof element === 'object' && 'type' in element;
-};
-
-export const isReactElements = (elements: ReactNode): elements is ReactElement[] => {
-  return !!elements && Array.isArray(elements) && elements.every(isReactElement);
-};
--- packages/forms/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './form-control';
-export * from './form-group';
-export * from './validators';
